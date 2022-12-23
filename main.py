72dd2932fc72c0355e3be1950241dae45f849012
MichalZnalezniak <mznalezn@amd.com>
2022-12-23 19:22:46 +0100

    STL10

--- main.py
+++ main.py
@@ -16,6 +16,42 @@ from sklearn.metrics import normalized_mutual_info_score
 from torch.utils.tensorboard import SummaryWriter
 import logging
 
+# train for one epoch to learn unique features
+def train_simclr(net, data_loader, train_optimizer, epoch):
+    mean_of_probs_per_level_per_epoch = {level: torch.zeros(2**level).cuda() for level in range(1, 5)}
+    net.train()
+    total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader)
+    total_tree_loss, total_reg_loss, total_simclr_loss = 0.0, 0.0, 0.0
+    for pos_1, pos_2, target in train_bar:
+        pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
+        feature_1, out_1, tree_output1 = net(pos_1)
+        feature_2, out_2, tree_output2 = net(pos_2)
+        # [2*B, D]
+        out = torch.cat([out_1, out_2], dim=0)
+        # [2*B, 2*B]
+        sim_matrix = torch.exp(torch.mm(out, out.t().contiguous()) / temperature)
+        mask = (torch.ones_like(sim_matrix) - torch.eye(2 * batch_size, device=sim_matrix.device)).bool()
+        # [2*B, 2*B-1]
+        sim_matrix = sim_matrix.masked_select(mask).view(2 * batch_size, -1)
+        # compute loss
+        pos_sim = torch.exp(torch.sum(out_1 * out_2, dim=-1) / temperature)
+        # [2*B]
+        pos_sim = torch.cat([pos_sim, pos_sim], dim=0)
+        loss_simclr = (- torch.log(pos_sim / sim_matrix.sum(dim=-1))).mean()
+        ### TREE LOSS
+        train_optimizer.zero_grad()
+
+        loss = loss_simclr
+        loss.backward()
+        train_optimizer.step()
+
+        total_num += batch_size
+        total_simclr_loss += loss_simclr.item() * batch_size
+        total_loss += loss.item() * batch_size
+        train_bar.set_description('Train Epoch: [{}/{}] Loss: {:.4f}'.format(epoch, epochs, total_loss / total_num))
+
+    return total_loss / total_num, 0, 0, 0
+
 
 # train for one epoch to learn unique features
 def train(net, data_loader, train_optimizer, epoch):
@@ -45,10 +81,9 @@ def train(net, data_loader, train_optimizer, epoch):
         ##
         ##       
         train_optimizer.zero_grad()
-        if epoch > 1000:
-            loss = loss_simclr + tree_loss_value + (2**(-4)*regularization_loss_value)
-        else:
-            loss = loss_simclr
+
+        loss =  loss_simclr + tree_loss_value + (2**(-4)*regularization_loss_value)
+
         loss.backward()
         train_optimizer.step()
 
@@ -187,10 +222,13 @@ if __name__ == '__main__':
         os.mkdir('results')
     best_nmi = 0.0
     for epoch in range(1, epochs + 1):
-        if epoch > 2:
+        if epoch > 1000:
+            total_loss = train_simclr(model, train_loader, optimizer, epoch)
             total_loss, tree_loss_train, reg_loss_train, simclr_loss_train = train(model, memory_loader, optimizer, epoch)
+
         else:
-            total_loss, tree_loss_train, reg_loss_train, simclr_loss_train = train(model, train_loader, optimizer, epoch)
+            total_loss, tree_loss_train, reg_loss_train, simclr_loss_train = train_simclr(model, train_loader, optimizer, epoch)
+
         results['train_loss'].append(total_loss)
         results['tree_loss_train'].append(tree_loss_train)
         results['reg_loss_train'].append(reg_loss_train)
