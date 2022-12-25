b3eef7c72ef34cc0386e686b983afd87a206aa33
MichalZnalezniak <mznalezn@amd.com>
2022-12-25 15:42:44 +0100

    TinyImageNet

--- utils.py
+++ utils.py
@@ -1,8 +1,26 @@
 from PIL import Image
 from torchvision import transforms
-from torchvision.datasets import CIFAR10, CIFAR100, STL10, ImageNet, MNIST, FashionMNIST
+import torchvision
+from torchvision.datasets import CIFAR10, STL10, ImageNet, MNIST, FashionMNIST
 from torch.utils.data import Subset
 import numpy as np
+import cv2
+
+class GaussianBlur:
+    def __init__(self, kernel_size, min=0.1, max=2.0):
+        self.min = min
+        self.max = max
+        self.kernel_size = kernel_size
+
+    def __call__(self, sample):
+        sample = np.array(sample)
+        prob = np.random.random_sample()
+        if prob < 0.5:
+            sigma = (self.max - self.min) * np.random.random_sample() + self.min
+            sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
+        return sample
+
+
 
 def get_transforms(name, args):
     train_transforms = {
@@ -13,15 +31,6 @@ def get_transforms(name, args):
                     transforms.RandomGrayscale(p=0.2),
                     transforms.ToTensor(),
                     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])]),
-
-        'cifar100': transforms.Compose([
-                    transforms.RandomResizedCrop(224 if args.cc_data else 32),
-                    transforms.RandomHorizontalFlip(p=0.5),
-                    transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-                    transforms.RandomGrayscale(p=0.2),
-                    transforms.ToTensor(),
-                    transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762])]),
-
         'stl10' : transforms.Compose([
                     transforms.RandomResizedCrop(224 if args.cc_data else 96),
                     transforms.RandomHorizontalFlip(p=0.5),
@@ -33,13 +42,23 @@ def get_transforms(name, args):
         'imagenet10': transforms.Compose([
                         transforms.RandomResizedCrop(224),
                         transforms.RandomHorizontalFlip(p=0.5),
-                        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+                        transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
                         transforms.RandomGrayscale(p=0.2),
+                        GaussianBlur(kernel_size=23),
                         transforms.ToTensor(),
                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
 
         'imagenetdogs': transforms.Compose([
                         transforms.RandomResizedCrop(224),
+                        transforms.RandomHorizontalFlip(p=0.5),
+                        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+                        transforms.RandomGrayscale(p=0.2),
+                        transforms.ToTensor(),
+                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+
+
+        'imagenettiny': transforms.Compose([
+                        transforms.RandomResizedCrop(224 if args.cc_data else 64),
                         transforms.RandomHorizontalFlip(p=0.5),
                         transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
                         transforms.RandomGrayscale(p=0.2),
@@ -53,7 +72,6 @@ def get_transforms(name, args):
                         transforms.RandomGrayscale(p=0.2),
                         transforms.ToTensor(),
                         transforms.Normalize((0.1307,), (0.3081,))]),
-
         'fmnist': transforms.Compose([
                         transforms.RandomResizedCrop(28),
                         transforms.RandomHorizontalFlip(p=0.5),
@@ -69,10 +87,6 @@ def get_transforms(name, args):
                     transforms.Resize(224 if args.cc_data else 32),
                     transforms.ToTensor(),
                     transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])]),
-        'cifar100': transforms.Compose([
-                        transforms.Resize(224 if args.cc_data else 32),
-                        transforms.ToTensor(),
-                        transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762])]),
         'stl10': transforms.Compose([
                     transforms.Resize(224 if args.cc_data else 96),
                     transforms.ToTensor(),
@@ -83,6 +97,10 @@ def get_transforms(name, args):
                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
         'imagenetdogs': transforms.Compose([
                         transforms.Resize((224, 224)),
+                        transforms.ToTensor(),
+                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        'imagenettiny': transforms.Compose([
+                        transforms.Resize(224 if args.cc_data else 64),
                         transforms.ToTensor(),
                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
         'mnist': transforms.Compose([
@@ -102,10 +120,10 @@ def get_transforms(name, args):
 def get_contrastive_dataset(name, args):
     train_data = {
         'cifar10': CIFAR10Pair(root='data', train=True, transform=get_transforms('cifar10', args)[0], download=True),
-        'cifar100': CIFAR100Pair(root='data', train=True, transform=get_transforms('cifar100', args)[0], download=True),
         'stl10': STL10Pair(root='data', split='unlabeled', transform=get_transforms('stl10', args)[0], download=True),
-        # 'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenet10', args)[0]), args), 
-        # 'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenetdogs', args)[0]), args), 
+        'imagenettiny':  ImageFolderPair(root='/shared/sets/datasets/tiny-imagenet-200/train', transform=get_transforms('imagenettiny', args)[0]),
+        'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenet10', args)[0]), args), 
+        'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenetdogs', args)[0]), args), 
         'mnist': MNISTPair(root='data', train=True, transform=get_transforms('mnist', args)[0], download=True),
         'fmnist': FashionMNISTPair(root='data', train=True, transform=get_transforms('fmnist', args)[0], download=True)
 
@@ -113,10 +131,10 @@ def get_contrastive_dataset(name, args):
     
     test_data = {
         'cifar10': CIFAR10Pair(root='data', train=False, transform=get_transforms('cifar10', args)[1], download=True),
-        'cifar100': CIFAR10Pair(root='data', train=False, transform=get_transforms('cifar100', args)[1], download=True),
         'stl10': STL10Pair(root='data', split='test', transform=get_transforms('stl10', args)[1], download=True),
-        # 'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='val', transform=get_transforms('imagenet10', args)[1]), args),
-        # 'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='val', transform=get_transforms('imagenetdogs', args)[1]), args),
+        'imagenettiny':  ImageFolderPair(root='/shared/sets/datasets/tiny-imagenet-200/val', transform=get_transforms('imagenettiny', args)[1]),
+        'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='val', transform=get_transforms('imagenet10', args)[1]), args),
+        'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='val', transform=get_transforms('imagenetdogs', args)[1]), args),
         'mnist': MNISTPair(root='data', train=False, transform=get_transforms('mnist', args)[1], download=True),
         'fmnist': FashionMNISTPair(root='data', train=False, transform=get_transforms('fmnist', args)[1], download=True)
 
@@ -127,10 +145,10 @@ def get_contrastive_dataset(name, args):
 
     memory_data = {
         'cifar10': CIFAR10Pair(root='data', train=True, transform=get_transforms('cifar10', args)[1], download=True),
-        'cifar100': CIFAR100Pair(root='data', train=True, transform=get_transforms('cifar100', args)[1], download=True),
         'stl10': STL10Pair(root='data', split='unlabeled', transform=get_transforms('stl10', args)[1], download=True),
-        # 'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenet10', args)[1]), args),
-        # 'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenetdogs', args)[1]), args),
+        'imagenettiny':  ImageFolderPair(root='/shared/sets/datasets/tiny-imagenet-200/train', transform=get_transforms('imagenettiny', args)[1]),
+        'imagenet10': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenet10', args)[1]), args),
+        'imagenetdogs': filter_ImageNet(ImageNetPair('/shared/sets/datasets/vision/ImageNet', split='train', transform=get_transforms('imagenetdogs', args)[1]), args),
         'mnist': MNISTPair(root='data', train=True, transform=get_transforms('mnist', args)[1], download=True),
         'fmnist': FashionMNISTPair(root='data', train=True, transform=get_transforms('fmnist', args)[1], download=True)
 
@@ -138,6 +156,18 @@ def get_contrastive_dataset(name, args):
     }
     return train_data[name], memory_data[name], test_data[name]
 
+
+class ImageFolderPair(torchvision.datasets.ImageFolder):
+  def __getitem__(self, index: int):
+        path, target = self.samples[index]
+        img = self.loader(path)
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return pos_1, pos_2, target
 
 def filter_ImageNet(image_dataset, args):
     if args.dataset_name == 'imagenet10':
@@ -252,24 +282,6 @@ class STL10Pair(STL10):
 
 
 class CIFAR10Pair(CIFAR10):
-    """CIFAR10 Dataset.
-    """
-
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-        img = Image.fromarray(img)
-
-        if self.transform is not None:
-            pos_1 = self.transform(img)
-            pos_2 = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return pos_1, pos_2, target
-
-
-class CIFAR100Pair(CIFAR100):
     """CIFAR10 Dataset.
     """
 
